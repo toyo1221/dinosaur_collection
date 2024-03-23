fc726fe5deb8ce801ff6e9ff5d8531a0bef9aeb9
toyo1221 <iyankukku1221@icloud.com>
2024-03-23 12:26:13 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'dinosaur#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :dinosaurs, only: :index do
+  end
 end
