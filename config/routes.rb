d546eb233d04091ba38dfc3adf1f19af3f672a56
toyo1221 <iyankukku1221@icloud.com>
2024-03-27 03:02:20 +0900

    詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'dinosaurs#index'
-  resources :dinosaurs, only: [:index, :new, :create]
+  resources :dinosaurs, only: [:index, :new, :create, :show]
 end
