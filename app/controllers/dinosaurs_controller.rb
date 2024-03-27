7ae739b039ccd004fd1b790acac5476b762947ba
toyo1221 <iyankukku1221@icloud.com>
2024-03-28 00:32:01 +0900

    恐竜編集機能の実装

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -1,5 +1,6 @@
 class DinosaursController < ApplicationController
-
+  before_action :authenticate_admin!, only: [:new, :create, :edit, :update]
+  before_action :find_dinosaur, only: [:show, :edit, :update]
   def index
     @dinosaurs = Dinosaur.order(:name)
   end
@@ -18,12 +19,26 @@ class DinosaursController < ApplicationController
   end
 
   def show
-    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def edit
+      @dinosaur.errors.clear
+  end
+
+  def update
+    if @dinosaur.update(dinosaur_params)
+      redirect_to dinosaur_path(@dinosaur.id)
+    else
+      render :edit,status: :unprocessable_entity
+    end
   end
 
   private
+  def find_dinosaur
+    @dinosaur = Dinosaur.find(params[:id])
+  end
 
   def dinosaur_params
-    params.require(:dinosaur).permit(:image, :name, :eating_id, :era_id, :size, :weight, :explanation)
+    params.require(:dinosaur).permit(:image, :name, :eating_id, :era_id, :size, :weight, :explanation).merge(admin_id:current_admin.id)
   end
 end
