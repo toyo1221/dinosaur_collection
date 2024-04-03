8917bdf8069f9b64d9883ab1d215afed6d5261a6
toyo1221 <iyankukku1221@icloud.com>
2024-04-03 13:16:17 +0900

    トピックス機能の修正と削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   root to: 'dinosaurs#index'
   resources :dinosaurs do
-    resources :topics, only: [:index, :new, :create]
+    resources :topics, only: :index
   end
+  resources :topics, only: [:new, :create, :destroy ]
 end
